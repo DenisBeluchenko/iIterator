1ac9740bbe77051d4beba9ebf948f2a0e35b5b28
Denis Beluchenko <dzibis@gmail.com>
2023-01-25 07:21:20 +0700

    final

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -8,15 +8,7 @@ public class Randoms implements Iterable<Integer> {
     public Randoms(int min, int max) {
         this.min = min;
         this.max = max + 1;
-        Random random1 = new Random();
-        int i;
-        while (true) {
-            i = random1.nextInt(max - min) + min;
-            if (i > min & i < max) {
-                this.random = random1;
-                break;
-            }
-        }
+        random = new Random();
     }
 
     @Override
